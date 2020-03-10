dbf6ea87fca1b31c790862a32fda4369cb54abdb
nickyc975 <chenjinlong2016@gmail.com>
2020-03-10 20:33:36 +0800

    Added VSCodeObject.

--- inc/objects/VSCodeObject.hpp
+++ inc/objects/VSCodeObject.hpp
@@ -1,59 +1,50 @@
 #ifndef VS_CODE_OBJECT_H
 #define VS_CODE_OBJECT_H
 
-#include "code.hpp"
+#include "opcode.hpp"
 #include "VSObject.hpp"
-#include "VSMapObject.hpp"
-#include "VSListObject.hpp"
+#include "VSTypeObject.hpp"
 
-typedef enum
-{
-    NORM_BLK,
-    FUNC_BLK,
-    LOOP_BLK
-} CODE_BLK_TYPE;
+#include <vector>
 
-static char *CODE_BLK_STR[] = {"NORM", "FUNC", "LOOP"};
-
-class VSInst : public VSObject
+class VSInst
 {
 public:
     OPCODE opcode;
-    // constant addr
     vs_addr_t operand;
 
     VSInst(OPCODE opcode);
     VSInst(OPCODE opcode, vs_addr_t operand);
+    ~VSInst() = default;
+
+    VSInst &operator=(VSInst &inst);
 };
 
 class VSCodeObject : public VSObject
 {
 public:
-    const std::string name;
-    const CODE_BLK_TYPE type;
+    vs_size_t ninsts;
+    vs_size_t nconsts;
+    vs_size_t nargs;
+    vs_size_t nlvars;
+    vs_size_t nfreevars;
 
-    vs_size_t inst_num;
-    vs_size_t const_num;
-    vs_size_t arg_num;
-    vs_size_t lvar_num;
-    vs_size_t nlvar_num;
-
-    // For loop block, this indicates the start point of the loop body.
-    vs_addr_t loop_start;
-
+    VSObject *name;
     VSObject *consts;
-    VSObject *code;
-    VSObject *local_names;
-    VSObject *non_local_names;
-    VSObject *name_to_addr;
+    VSObject *lvars;
+    VSObject *freevars;
+    std::vector<VSInst> code;
 
-    VSCodeObject(std::string name, CODE_BLK_TYPE type);
+    VSCodeObject(VSObject *name);
+    ~VSCodeObject() = default;
 
     void add_inst(VSInst inst);
     void add_const(VSObject *object);
-    void add_arg(std::string name);
-    void add_local(std::string name);
-    void add_non_local(std::string name);
+    void add_arg(VSObject *name);
+    void add_lvar(VSObject *name);
+    void add_freevar(VSObject *name);
 };
+
+extern VSTypeObject *VSCodeType;
 
 #endif
--- src/objects/VSCodeObject.cpp
+++ src/objects/VSCodeObject.cpp
@@ -1,71 +1,148 @@
-#include "vs.hpp"
+#include "error.hpp"
+#include "objects/VSNoneObject.hpp"
+#include "objects/VSBoolObject.hpp"
+#include "objects/VSListObject.hpp"
+#include "objects/VSTupleObject.hpp"
+#include "objects/VSStringObject.hpp"
+#include "objects/VSCodeObject.hpp"
 
-VSInst::VSInst(OPCODE opcode): opcode(opcode), operand(0)
+VSInst::VSInst(OPCODE opcode) : opcode(opcode), operand(0)
 {
 }
 
-VSInst::VSInst(OPCODE opcode, vs_addr_t operand): opcode(opcode), operand(operand)
+VSInst::VSInst(OPCODE opcode, vs_addr_t operand) : opcode(opcode), operand(operand)
 {
 }
 
-const char *VSInst::to_bytes()
+VSInst &VSInst::operator=(VSInst &inst)
 {
-    return this->to_string().c_str();
+    this->opcode = inst.opcode;
+    this->operand = inst.operand;
+    return *this;
 }
 
-const std::string VSInst::to_string()
+VSCodeObject::VSCodeObject(VSObject *name)
 {
-    return std::string(OPCODE_STR[this->opcode]) + "\t" + std::to_string(this->operand);
-}
+    this->name = name;
+    INCREF(name);
 
-VSCodeObject::VSCodeObject(std::string name, CODE_BLK_TYPE type) : name(name), type(type)
-{
-    this->inst_num = 0;
-    this->const_num = 0;
-    this->arg_num = 0;
-    this->lvar_num = 0;
-    this->nlvar_num = 0;
-    this->loop_start = 0;
+    this->ninsts = 0;
+    this->nconsts = 0;
+    this->nargs = 0;
+    this->nlvars = 0;
+    this->nfreevars = 0;
 
-    this->consts = VSObjectList();
+    this->consts = vs_list_pack(0);
+    this->lvars = vs_list_pack(0);
+    this->freevars = vs_list_pack(0);
     this->code = std::vector<VSInst>();
-    this->local_names = std::vector<std::string>();
-    this->non_local_names = std::vector<std::string>();
-    this->name_to_addr = std::unordered_map<std::string, vs_addr_t>();
 
     // set constants
-    this->add_const(new VSObject(VSValue::None()));
-    this->add_const(new VSObject(VSValue::True()));
-    this->add_const(new VSObject(VSValue::False()));
+    this->add_const(VS_NONE);
+    this->add_const(VS_TRUE);
+    this->add_const(VS_FALSE);
 }
 
 void VSCodeObject::add_inst(VSInst inst)
 {
     this->code.push_back(inst);
-    this->inst_num++;
+    this->ninsts++;
 }
 
 void VSCodeObject::add_const(VSObject *object)
 {
-    this->consts.push(*object);
-    this->const_num++;
+    VSObject::appenditem(this->consts, object);
+    this->nconsts++;
 }
 
-void VSCodeObject::add_arg(std::string name)
+void VSCodeObject::add_arg(VSObject *name)
 {
-    this->add_local(name);
-    this->arg_num++;
+    this->add_lvar(name);
+    this->nargs++;
 }
 
-void VSCodeObject::add_local(std::string name)
+void VSCodeObject::add_lvar(VSObject *name)
 {
-    this->name_to_addr[name] = this->lvar_num;
-    this->local_names.push_back(name);
-    this->lvar_num++;
+    VSObject::appenditem(this->lvars, name);
+    this->nlvars++;
 }
 
-void VSCodeObject::add_non_local(std::string name)
+void VSCodeObject::add_freevar(VSObject *name)
 {
-    this->non_local_names.push_back(name);
-    this->nlvar_num++;
+    VSObject::appenditem(this->freevars, name);
+    this->nfreevars++;
 }
+
+VSObject *vs_code_new(VSObject *typeobj, VSObject *args, VSObject *)
+{
+    VSTypeObject *ttype = VS_TYPEOF(typeobj);
+    VS_ENSURE_TYPE(ttype, T_TYPE, "code new");
+
+    VSTypeObject *type = VS_AS_TYPE(typeobj);
+    VS_ENSURE_TYPE(type, T_CODE, "code new");
+
+    VSObject *name;
+    if (!vs_tuple_unpack(args, 1, &name))
+    {
+        err("code.__new__() expected 1 arg but got %lld\n", VSObject::c_getlen(args));
+        return NULL;
+    }
+
+    VSTypeObject *nametype = VS_TYPEOF(name);
+    VS_ENSURE_TYPE(nametype, T_STR, "code.__new__()");
+
+    INCREF_RET(VS_AS_OBJECT(new VSCodeObject(name)));
+}
+
+void vs_code_init(VSObject *, VSObject *, VSObject *)
+{
+}
+
+void vs_code_clear(VSObject *codeobj)
+{
+    VSTypeObject *type = VS_TYPEOF(codeobj);
+    VS_ENSURE_TYPE(type, T_CODE, "code.__init__()");
+
+    VSCodeObject *code = (VSCodeObject *)codeobj;
+    DECREF(code->consts);
+    DECREF(code->lvars);
+    DECREF(code->freevars);
+}
+
+VSObject *vs_code_str(VSObject *codeobj)
+{
+    VSTypeObject *type = VS_TYPEOF(codeobj);
+    VS_ENSURE_TYPE(type, T_CODE, "code.__str__()");
+
+    return vs_string_from_cstring("code");
+}
+
+VSObject *vs_code_bytes(VSObject *codeobj)
+{
+    VSTypeObject *type = VS_TYPEOF(codeobj);
+    VS_ENSURE_TYPE(type, T_CODE, "code.__bytes__()");
+
+    return NULL;
+}
+
+VSTypeObject *VSCodeType = new VSTypeObject(
+    T_CODE,
+    "code",                  // __name__
+    NULL,                    // __attrs__
+    vs_code_new,             // __new__
+    vs_code_init,            // __init__
+    NULL,                    // __copy__
+    vs_code_clear,           // __clear__
+    NULL,                    // __getattr__
+    NULL,                    // __hasattr__
+    NULL,                    // __setattr__
+    NULL,                    // __removeattr__
+    vs_hash_not_implemented, // __hash__
+    NULL,                    // __lt__
+    vs_default_eq,           // __eq__
+    vs_code_str,             // __str__
+    vs_code_bytes,           // __bytes__
+    NULL,                    // __call__
+    NULL,                    // _number_funcs
+    NULL                     // _container_funcs
+);
