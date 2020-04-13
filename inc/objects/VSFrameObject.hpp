965bab919e97572fbd7063df25b0dd6d509edcf3
nickyc975 <chenjinlong2016@gmail.com>
2020-04-13 17:38:20 +0800

    Improved VSFrameObject.

--- inc/objects/VSFrameObject.hpp
+++ inc/objects/VSFrameObject.hpp
@@ -17,13 +17,21 @@ public:
     VSTupleObject *locals;
 
     VSFrameObject(VSFunctionObject *func);
-    ~VSFrameObject();
+    ~VSFrameObject() = default;
 };
 
 #define AS_FRAME(obj) ((VSFrameObject *)(obj))
 #define FRAME_GET_FUNC(frame) (AS_FRAME(frame)->func)
 #define FRAME_GET_NLOCALS(frame) (TUPLE_LEN(AS_FRAME(frame)->locals))
+
 #define FRAME_GET_LOCAL(frame, idx) (VS_CELL_GET((TUPLE_GET(AS_FRAME(frame)->locals, (idx)))))
+#define FRAME_GET_LOCAL_CELL(frame, idx) (TUPLE_GET(AS_FRAME(frame)->locals, (idx)))
 #define FRAME_SET_LOCAL(frame, idx, val) (VS_CELL_SET((TUPLE_GET(AS_FRAME(frame)->locals, (idx))), val))
+#define FRAME_SET_LOCAL_CELL(frame, idx, cell) (TUPLE_SET(AS_FRAME(frame)->locals, (idx), (cell)))
+
+#define FRAME_GET_FREE(frame, idx) (VS_CELL_GET(TUPLE_GET(FUNC_GET_FREEVARS((AS_FRAME(frame))->func), (idx))))
+#define FRAME_GET_FREE_CELL(frame, idx) (TUPLE_GET(FUNC_GET_FREEVARS((AS_FRAME(frame))->func), (idx)))
+
+#define FRAME_GET_BUILTIN(frame, idx) (TUPLE_GET(FUNC_GET_BUILTINS((AS_FRAME(frame))->func), (idx)))
 
 #endif
--- src/objects/VSFrameObject.cpp
+++ src/objects/VSFrameObject.cpp
@@ -1,35 +1,28 @@
-#include "objects/VSNoneObject.hpp"
-#include "objects/VSStringObject.hpp"
-#include "objects/VSCodeObject.hpp"
 #include "objects/VSFrameObject.hpp"
 
-VSFrameObject::VSFrameObject(VSFunctionObject *func)
-{
+#include "objects/VSCodeObject.hpp"
+#include "objects/VSNoneObject.hpp"
+#include "objects/VSStringObject.hpp"
+
+VSFrameObject::VSFrameObject(VSFunctionObject *func) {
     this->pc = 0;
     this->func = func;
     this->nlocals = AS_CODE(FUNC_GET_CODE(this->func))->nlvars;
     this->locals = new VSTupleObject(this->nlocals);
 
-    for (vs_size_t i = 0; i < this->nlocals; i++)
-    {
+    for (vs_size_t i = 0; i < this->nlocals; i++) {
         TUPLE_SET(this->locals, i, new VSCellObject(VS_NONE));
     }
 }
 
-VSFrameObject::~VSFrameObject()
-{
-}
-
-VSObject *vs_frame_str(VSObject *frameobj)
-{
+VSObject *vs_frame_str(VSObject *frameobj) {
     VSTypeObject *type = VS_TYPEOF(frameobj);
     VS_ENSURE_TYPE(type, T_FRAME, "frame str");
 
     INCREF_RET(C_STRING_TO_STRING("frame"));
 }
 
-VSObject *vs_frame_bytes(VSObject *frameobj)
-{
+VSObject *vs_frame_bytes(VSObject *frameobj) {
     VSTypeObject *type = VS_TYPEOF(frameobj);
     VS_ENSURE_TYPE(type, T_FRAME, "frame bytes");
 
@@ -39,22 +32,22 @@ VSObject *vs_frame_bytes(VSObject *frameobj)
 VSTypeObject *VSFrameType = new VSTypeObject(
     VSTypeType,
     T_FRAME,
-    "frame",            // __name__
-    NULL,               // __attrs__
-    NULL,       // __new__
-    NULL,      // __init__
-    NULL,      // __copy__
-    NULL,               // __clear__
-    NULL,               // __getattr__
-    NULL,               // __hasattr__
-    NULL,               // __setattr__
-    NULL,               // __removeattr__
-    vs_hash_not_implemented,      // __hash__
-    NULL,        // __lt__
-    vs_default_eq,        // __eq__
-    vs_frame_str,       // __str__
-    vs_frame_bytes,     // __bytes__
-    NULL,               // __call__
-    NULL, // _number_funcs
-    NULL                // _container_funcs
+    "frame",                  // __name__
+    NULL,                     // __attrs__
+    NULL,                     // __new__
+    NULL,                     // __init__
+    NULL,                     // __copy__
+    NULL,                     // __clear__
+    NULL,                     // __getattr__
+    NULL,                     // __hasattr__
+    NULL,                     // __setattr__
+    NULL,                     // __removeattr__
+    vs_hash_not_implemented,  // __hash__
+    NULL,                     // __lt__
+    vs_default_eq,            // __eq__
+    vs_frame_str,             // __str__
+    vs_frame_bytes,           // __bytes__
+    NULL,                     // __call__
+    NULL,                     // _number_funcs
+    NULL                      // _container_funcs
 );
