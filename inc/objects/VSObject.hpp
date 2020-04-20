50474111808a58de67769f578e0f21453a6382b6
nickyc975 <chenjinlong2016@gmail.com>
2020-04-20 15:30:55 +0800

    Refactored VSObject.

--- inc/objects/VSObject.hpp
+++ inc/objects/VSObject.hpp
@@ -1,160 +1,56 @@
 #ifndef VS_OBJECTS
 #define VS_OBJECTS
 
-#include "vs.hpp"
-
 #include <cstdlib>
 
+#include "vs.hpp"
+
 typedef enum {
-    T_TYPE,
-    T_CLASS,
     T_NONE,
     T_BOOL,
     T_CHAR,
     T_INT,
     T_FLOAT,
     T_STR,
-    T_CELL,
     T_LIST,
     T_TUPLE,
     T_MAP,
     T_SET,
-    T_CODE,
-    T_FRAME,
     T_FUNC,
-    T_OBJECT
-} TYPE_TYPE;
+    T_OBJECT,
+    T_CELL,
+    T_CODE,
+    T_FRAME
+} TYPE;
 
-class VSObject {
-public:
-    VSObject *type;
-    vs_size_t refcnt;
+char *TYPE_STR[] = {
+    "none",
+    "bool",
+    "char",
+    "int",
+    "float",
+    "str",
+    "bytes",
+    "list",
+    "tuple",
+    "map",
+    "set",
+    "func",
+    "object",
+    "cell",
+    "code",
+    "frame"};
 
-    VSObject() {
-        this->type = NULL;
-        this->refcnt = 0;
-    }
-};
-
-typedef VSObject *(*unaryfunc)(VSObject *);
-typedef VSObject *(*binaryfunc)(VSObject *, VSObject *);
-typedef VSObject *(*ternaryfunc)(VSObject *, VSObject *, VSObject *);
-typedef VSObject *(*quaternaryfunc)(VSObject *, VSObject *, VSObject *, VSObject *);
-
-typedef VSObject *(*const_unaryfunc)(const VSObject *);
-typedef VSObject *(*const_binaryfunc)(const VSObject *, const VSObject *);
-typedef VSObject *(*const_ternaryfunc)(const VSObject *, const VSObject *, const VSObject *);
-typedef VSObject *(*const_quaternaryfunc)(const VSObject *, const VSObject *, const VSObject *, const VSObject *);
-
-typedef void (*void_unaryfunc)(VSObject *);
-typedef void (*void_binaryfunc)(VSObject *, VSObject *);
-typedef void (*void_ternaryfunc)(VSObject *, VSObject *, VSObject *);
-typedef void (*void_quaternaryfunc)(VSObject *, VSObject *, VSObject *, VSObject *);
-
-typedef void (*void_const_unaryfunc)(const VSObject *);
-typedef void (*void_const_binaryfunc)(const VSObject *, const VSObject *);
-typedef void (*void_const_ternaryfunc)(const VSObject *, const VSObject *, const VSObject *);
-typedef void (*void_const_quaternaryfunc)(const VSObject *, const VSObject *, const VSObject *, const VSObject *);
-
-class NumberFuncs {
-public:
-    unaryfunc __not__;
-    unaryfunc __neg__;
-    binaryfunc __add__;
-    binaryfunc __sub__;
-    binaryfunc __mul__;
-    binaryfunc __div__;
-    binaryfunc __mod__;
-    binaryfunc __and__;
-    binaryfunc __or__;
-
-    // type conversion
-    unaryfunc __bool__;
-    unaryfunc __char__;
-    binaryfunc __int__;
-    unaryfunc __float__;
-
-    NumberFuncs(
-        unaryfunc __not__,
-        unaryfunc __neg__,
-        binaryfunc __add__,
-        binaryfunc __sub__,
-        binaryfunc __mul__,
-        binaryfunc __div__,
-        binaryfunc __mod__,
-        binaryfunc __and__,
-        binaryfunc __or__,
-        unaryfunc __bool__,
-        unaryfunc __char__,
-        binaryfunc __int__,
-        unaryfunc __float__
-    ) : __not__(__not__),
-        __neg__(__neg__),
-        __add__(__add__),
-        __sub__(__sub__),
-        __mul__(__mul__),
-        __div__(__div__),
-        __mod__(__mod__),
-        __and__(__and__),
-        __or__(__or__),
-        __bool__(__bool__),
-        __char__(__char__),
-        __int__(__int__),
-        __float__(__float__)
-    {
-    }
-};
-
-class ContainerFuncs {
-public:
-    unaryfunc __len__;
-    binaryfunc __get__;
-    void_ternaryfunc __set__;
-    void_binaryfunc __append__;
-    binaryfunc __has__;
-    binaryfunc __has_at__;
-    void_binaryfunc __remove__;
-    void_binaryfunc __remove_at__;
-
-    ContainerFuncs(
-        unaryfunc __len__,
-        binaryfunc __get__,
-        void_ternaryfunc __set__,
-        void_binaryfunc __append__,
-        binaryfunc __has__,
-        binaryfunc __has_at__,
-        void_binaryfunc __remove__,
-        void_binaryfunc __remove_at__
-    ) : __len__(__len__),
-        __get__(__get__),
-        __set__(__set__),
-        __append__(__append__),
-        __has__(__has__),
-        __has_at__(__has_at__),
-        __remove__(__remove__),
-        __remove_at__(__remove_at__)
-    {
-    }
-};
-
-VSObject *vs_hash_not_implemented(const VSObject *obj);
-VSObject *vs_default_eq(const VSObject *a, const VSObject *b);
-
-inline VSObject *_NEW_REF(VSObject *obj) {
-    obj->refcnt++;
-    return obj;
-}
-
-#define VS_TYPEOF(obj) ((VSTypeObject *)(obj->type))
+class VSObject;
 
 #define VS_AS_OBJECT(obj) ((VSObject *)obj)
 
-#define VS_AS_TYPE(obj) ((VSTypeObject *)obj)
+#define VS_IS_TYPE(obj, ttype) (VS_AS_OBJECT(obj)->type == ttype)
 
-#define VS_ENSURE_TYPE(type, ttype, op)                                          \
-    if (type->t_type != ttype) {                                                 \
-        err("Can not apply \"" op "\" on type \"%s\".", type->__name__.c_str()); \
-        terminate(TERM_ERROR);                                                   \
+#define VS_ENSURE_TYPE(obj, ttype, op)                                        \
+    if (!VS_IS_TYPE(obj, ttype)) {                                            \
+        err("Can not apply \"" op "\" on type \"%s\".", TYPE_STR[obj->type]); \
+        terminate(TERM_ERROR);                                                \
     }
 
 #define INCREF(obj)                      \
@@ -173,32 +69,75 @@ inline VSObject *_NEW_REF(VSObject *obj) {
 
 #define NEW_REF(type, obj) (type) _NEW_REF(VS_AS_OBJECT(obj))
 
-#define DECREF(obj)                                                                  \
-    do {                                                                             \
-        auto _obj = obj;                                                             \
-        if (_obj != NULL) {                                                          \
-            VS_AS_OBJECT(_obj)->refcnt--;                                            \
-            if (VS_AS_OBJECT(_obj)->refcnt == 0) {                                   \
-                if (VS_TYPEOF(_obj) != NULL && VS_TYPEOF(_obj)->__clear__ != NULL) { \
-                    VS_TYPEOF(_obj)->__clear__(_obj);                                \
-                }                                                                    \
-                delete _obj;                                                         \
-            }                                                                        \
-        }                                                                            \
+#define DECREF(obj)                                \
+    do {                                           \
+        auto _obj = obj;                           \
+        if (_obj != NULL) {                        \
+            VS_AS_OBJECT(_obj)->refcnt--;          \
+            if (VS_AS_OBJECT(_obj)->refcnt == 0) { \
+                delete _obj;                       \
+            }                                      \
+        }                                          \
     } while (0);
 
-#define DECREF_EX(obj)                                                             \
-    do {                                                                           \
-        if (obj != NULL) {                                                         \
-            VS_AS_OBJECT(obj)->refcnt--;                                           \
-            if (VS_AS_OBJECT(obj)->refcnt == 0) {                                  \
-                if (VS_TYPEOF(obj) != NULL && VS_TYPEOF(obj)->__clear__ != NULL) { \
-                    VS_TYPEOF(obj)->__clear__(obj);                                \
-                }                                                                  \
-                delete obj;                                                        \
-                obj = NULL;                                                        \
-            }                                                                      \
-        }                                                                          \
+#define DECREF_EX(obj)                            \
+    do {                                          \
+        if (obj != NULL) {                        \
+            VS_AS_OBJECT(obj)->refcnt--;          \
+            if (VS_AS_OBJECT(obj)->refcnt == 0) { \
+                delete obj;                       \
+                obj = NULL;                       \
+            }                                     \
+        }                                         \
     } while (0);
+
+class AttributeDef {
+public:
+    bool readonly;
+    VSObject *attribute;
+
+    AttributeDef(bool readonly, VSObject *attribute) {
+        this->readonly = readonly;
+        this->attribute = attribute;
+        INCREF(this->attribute);
+    }
+
+    ~AttributeDef() {
+        DECREF_EX(this->attribute);
+    }
+};
+
+typedef std::unordered_map<std::string, AttributeDef> str_attr_map;
+
+class VSObject {
+public:
+    TYPE type;
+    vs_size_t refcnt;
+    str_attr_map attrs;
+
+    VSObject() {
+        this->refcnt = 0;
+        attrs = str_attr_map();
+    }
+};
+
+inline VSObject *_NEW_REF(VSObject *obj) {
+    if (obj != NULL) {
+        obj->refcnt++;
+    }
+    return obj;
+}
+
+typedef VSObject *(*unaryfunc)(VSObject *);
+typedef VSObject *(*binaryfunc)(VSObject *, VSObject *);
+typedef VSObject *(*ternaryfunc)(VSObject *, VSObject *, VSObject *);
+typedef VSObject *(*quaternaryfunc)(VSObject *, VSObject *, VSObject *, VSObject *);
+
+typedef VSObject *(*const_unaryfunc)(const VSObject *);
+typedef VSObject *(*const_binaryfunc)(const VSObject *, const VSObject *);
+typedef VSObject *(*const_ternaryfunc)(const VSObject *, const VSObject *, const VSObject *);
+typedef VSObject *(*const_quaternaryfunc)(const VSObject *, const VSObject *, const VSObject *, const VSObject *);
+
+VSObject *vs_default_eq(const VSObject *a, const VSObject *b);
 
 #endif
--- src/objects/VSObject.cpp
+++ src/objects/VSObject.cpp
@@ -1,16 +1,7 @@
-#include "error.hpp"
-#include "objects/VSIntObject.hpp"
-#include "objects/VSBoolObject.hpp"
 #include "objects/VSObject.hpp"
 
-VSObject *vs_hash_not_implemented(const VSObject *obj)
-{
-    err("%s.__hash__() not implemented.", VS_TYPEOF(obj)->__name__.c_str());
-    terminate(TERM_ERROR);
-    INCREF_RET(VS_ZERO);
-}
+#include "objects/VSBoolObject.hpp"
 
-VSObject *vs_default_eq(const VSObject *a, const VSObject *b)
-{
+VSObject *vs_default_eq(const VSObject *a, const VSObject *b) {
     return C_BOOL_TO_BOOL(a == b);
 }
