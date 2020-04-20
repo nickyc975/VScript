fa4f94229dac6f5f4b25a1876f29d9714dc4539a
nickyc975 <chenjinlong2016@gmail.com>
2020-04-20 16:49:33 +0800

    Re-designed VSFunctionObject.

--- inc/objects/VSFunctionObject.hpp
+++ inc/objects/VSFunctionObject.hpp
@@ -1,64 +1,53 @@
 #ifndef VS_FUNCTION_H
 #define VS_FUNCTION_H
 
+#include "VSCodeObject.hpp"
+#include "VSNoneObject.hpp"
 #include "VSObject.hpp"
-#include "VSTypeObject.hpp"
-
-extern VSTypeObject *VSFunctionType;
+#include "VSStringObject.hpp"
+#include "VSTupleObject.hpp"
 
 class VSFunctionObject : public VSObject {
 public:
-    /* | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
-     * |c|r|nargs|m|                      unused                       |
-     */
-    unsigned int flags;
-    VSObject *name;
-    VSObject *code;
-    VSObject *cellvars;
-    VSObject *freevars;
-    VSObject *defaults;
+    VSStringObject *name;
 
-    // for builtin native functions
-    void *cfunc;
+    VSFunctionObject();
+    ~VSFunctionObject();
 
-    VSFunctionObject(VSObject *name, void *cfunc, int nargs, bool has_retval, bool is_method);
-    VSFunctionObject(VSObject *name, VSObject *code, VSObject *cellvars, VSObject *freevars, VSObject *defaults, bool is_method);
-    ~VSFunctionObject() = default;
+    virtual VSObject *call(VSTupleObject *);
 };
 
-#define AS_FUNC(func) ((VSFunctionObject *)func)
+class VSNativeFunctionObject : public VSFunctionObject {
+private:
+    void *cfunc;
+    cint_t nargs;
+    bool const_args;
+    VSObject *bind_obj;
 
-#define FUNC_IS_C_FUNC(func) ((AS_FUNC(func)->flags >> 31) & 1)
-#define FUNC_SET_IS_C_FUNC(func) (AS_FUNC(func)->flags | (1 << 31))
-#define FUNC_CLEAR_IS_C_FUNC(func) (AS_FUNC(func)->flags & ~(1 << 31))
+public:
+    VSNativeFunctionObject(VSStringObject *name, void *cfunc, cint_t nargs, bool const_args, VSObject *bind_obj);
+    ~VSNativeFunctionObject();
 
-#define FUNC_HAS_RETVAL(func) ((AS_FUNC(func)->flags >> 30) & 1)
-#define FUNC_SET_HAS_RETVAL(func) (AS_FUNC(func)->flags | (1 << 30))
-#define FUNC_CLEAR_HAS_REVAL(func) (AS_FUNC(func)->flags & ~(1 << 30))
+    VSObject *call(VSTupleObject *args) override;
+};
 
-#define FUNC_GET_NARGS(func) ((AS_FUNC(func)->flags >> 27) & 7)
-#define FUNC_SET_NARGS(func, v) (AS_FUNC(func)->flags | (v << 27))
+class VSDynamicFunctionObject : public VSFunctionObject {
+private:
+    VSCodeObject *code;
+    VSTupleObject *cellvars;
+    VSTupleObject *freevars;
+    VSTupleObject *builtins;
 
-#define FUNC_IS_METHOD(func) ((AS_FUNC(func)->flags >> 26) & 1)
-#define FUNC_SET_IS_METHOD(func) (AS_FUNC(func)->flags | (1 << 26))
-#define FUNC_CLEAR_IS_METHOD(func) (AS_FUNC(func)->flags & ~(1 << 26))
+public:
+    VSDynamicFunctionObject(
+        VSStringObject *name,
+        VSCodeObject *code,
+        VSTupleObject *cellvars,
+        VSTupleObject *freevars,
+        VSTupleObject *builtins);
+    ~VSDynamicFunctionObject();
 
-#define FUNC_GET_NAME(func) (AS_FUNC(func)->name)
-#define FUNC_SET_NAME(func, fname) (AS_FUNC(func)->name = fname)
-
-#define FUNC_GET_CODE(func) (AS_FUNC(func)->code)
-#define FUNC_SET_CODE(func, fcode) (AS_FUNC(func)->code = fcode)
-
-#define FUNC_GET_CELLVARS(func) (AS_FUNC(func)->cellvars)
-#define FUNC_SET_CELLVARS(func, fcellvars) (AS_FUNC(func)->cellvars = fcellvars)
-
-#define FUNC_GET_FREEVARS(func) (AS_FUNC(func)->freevars)
-#define FUNC_SET_FREEVARS(func, ffreevars) (AS_FUNC(func)->freevars = ffreevars)
-
-#define FUNC_GET_DEFAULTS(func) (AS_FUNC(func)->defaults)
-#define FUNC_SET_DEFAULTS(func, fdefaults) (AS_FUNC(func)->defaults = fdefaults)
-
-#define FUNC_GET_C_FUNC(func) (AS_FUNC(func)->cfunc)
-#define FUNC_SET_C_FUNC(func, fcfunc) (AS_FUNC(func)->cfunc = fcfunc)
+    VSObject *call(VSTupleObject *args) override;
+};
 
 #endif
--- src/objects/VSFunctionObject.cpp
+++ src/objects/VSFunctionObject.cpp
@@ -1,102 +1,169 @@
 #include "objects/VSFunctionObject.hpp"
 
 #include <cassert>
+#include <stack>
 
 #include "error.hpp"
-#include "objects/VSNoneObject.hpp"
-#include "objects/VSStringObject.hpp"
+#include "objects/VSFrameObject.hpp"
 
-VSFunctionObject::VSFunctionObject(VSObject *name, void *cfunc, int nargs, bool has_retval, bool is_method) {
-    assert(name != NULL);
+#define ERR_NARGS(expected, actual)                                \
+    err("Unexpected nargs: %ld, expected: %ld", actual, expected); \
+    terminate(TERM_ERROR);
 
-    this->type = VSFunctionType;
-    VS_ENSURE_TYPE(VS_TYPEOF(name), T_STR, "as function name");
-    this->name = NEW_REF(VSObject *, name);
-    this->type = VSFunctionType;
-
-    this->flags = 0;
-
-    assert(cfunc != NULL);
-    this->cfunc = cfunc;
-    FUNC_SET_IS_C_FUNC(this);
-
-    if (nargs < 1 || nargs > 4) {
-        err("invalid internal function arg count: %d\n", nargs);
-        terminate(TERM_ERROR);
-    }
-    FUNC_SET_NARGS(this, nargs);
-
-    if (has_retval) {
-        FUNC_SET_HAS_RETVAL(this);
-    }
-
-    if (is_method) {
-        FUNC_SET_IS_METHOD(this);
-    }
-}
-
-VSFunctionObject::VSFunctionObject(VSObject *name, VSObject *code, VSObject *cellvars, VSObject *freevars, VSObject *defaults, bool is_method) {
-    assert(name != NULL);
-
-    this->type = VSFunctionType;
-    VS_ENSURE_TYPE(VS_TYPEOF(name), T_STR, "as function name");
-    this->name = NEW_REF(VSObject *, name);
-    this->type = VSFunctionType;
-
-    this->flags = 0;
-
-    assert(code != NULL);
-    VS_ENSURE_TYPE(VS_TYPEOF(code), T_CODE, "as function code");
-    this->code = NEW_REF(VSObject *, code);
-
-    if (cellvars != NULL) {
-        this->cellvars = NEW_REF(VSObject *, cellvars);
-    }
-
-    if (freevars != NULL) {
-        this->freevars = NEW_REF(VSObject *, freevars);
-    }
-
-    this->defaults = NEW_REF(VSObject *, defaults == NULL ? VS_NONE : defaults);
-
-    if (is_method) {
-        FUNC_SET_IS_METHOD(this);
-    }
-}
-
+/* begin function attributes */
 VSObject *vs_func_str(VSObject *funcobj) {
-    VSTypeObject *type = VS_TYPEOF(funcobj);
-    VS_ENSURE_TYPE(type, T_FUNC, "function str");
+    VS_ENSURE_TYPE(funcobj, T_FUNC, "func.__str__()");
 
-    INCREF_RET(C_STRING_TO_STRING("function"));
+    VSFunctionObject *func = (VSFunctionObject *)funcobj;
+    INCREF_RET(C_STRING_TO_STRING("function: " + func->name->_value));
 }
 
 VSObject *vs_func_bytes(VSObject *funcobj) {
-    VSTypeObject *type = VS_TYPEOF(funcobj);
-    VS_ENSURE_TYPE(type, T_FUNC, "function bytes");
+    VS_ENSURE_TYPE(funcobj, T_FUNC, "func.__bytes__()");
 
-    return NULL;
+    INCREF_RET(VS_NONE);
+}
+/* end function attributes */
+
+/* Base function type definition */
+VSFunctionObject::VSFunctionObject() {
+    this->type = T_FUNC;
+    this->attrs["__eq__"] = AttributeDef(true, new VSNativeFunctionObject(
+                                                   C_STRING_TO_STRING("__eq__"), (void *)vs_default_eq, 2, true, this));
+    this->attrs["__str__"] = AttributeDef(true, new VSNativeFunctionObject(
+                                                    C_STRING_TO_STRING("__str__"), (void *)vs_func_str, 1, false, this));
+    this->attrs["__bytes__"] = AttributeDef(true, new VSNativeFunctionObject(
+                                                      C_STRING_TO_STRING("__bytes__"), (void *)vs_func_bytes, 1, false, this));
 }
 
-VSTypeObject *VSFunctionType = new VSTypeObject(
-    VSTypeType,
-    T_FUNC,
-    "function",               // __name__
-    NULL,                     // __attrs__
-    NULL,                     // __new__
-    NULL,                     // __init__
-    NULL,                     // __copy__
-    NULL,                     // __clear__
-    NULL,                     // __getattr__
-    NULL,                     // __hasattr__
-    NULL,                     // __setattr__
-    NULL,                     // __removeattr__
-    vs_hash_not_implemented,  // __hash__
-    NULL,                     // __lt__
-    vs_default_eq,            // __eq__
-    vs_func_str,              // __str__
-    vs_func_bytes,            // __bytes__
-    NULL,                     // __call__
-    NULL,                     // _number_funcs
-    NULL                      // _container_funcs
-);
+VSFunctionObject::~VSFunctionObject() {
+}
+
+VSObject *VSFunctionObject::call(VSTupleObject *args) {
+    INCREF_RET(VS_NONE);
+}
+
+/* Native function type definition */
+VSNativeFunctionObject::VSNativeFunctionObject(VSStringObject *name, void *cfunc, cint_t nargs, bool const_args, VSObject *bind_obj) {
+    assert(name != NULL);
+
+    this->name = NEW_REF(VSStringObject *, name);
+    this->cfunc = cfunc;
+    this->nargs = nargs;
+    this->const_args = const_args;
+    this->bind_obj = NEW_REF(VSObject *, bind_obj);
+}
+
+VSNativeFunctionObject::~VSNativeFunctionObject() {
+    DECREF_EX(this->name);
+    DECREF_EX(this->bind_obj);
+}
+
+VSObject *VSNativeFunctionObject::call(VSTupleObject *args) {
+    assert(args != NULL);
+    VS_ENSURE_TYPE(args, T_TUPLE, "as args");
+
+    bool is_method = this->bind_obj != NULL;
+
+    cint_t expected_nargs = this->nargs;
+    if (is_method) {
+        expected_nargs--;
+        if (expected_nargs < 0) {
+            ERR_NARGS(0, 1);
+        }
+    }
+
+    cint_t actual_nargs = (cint_t)TUPLE_LEN(args);
+    if (expected_nargs != actual_nargs) {
+        ERR_NARGS(expected_nargs, actual_nargs);
+    }
+
+    VSObject *res = NULL;
+    switch (this->nargs) {
+        case 1: {
+            VSObject *arg0 = is_method ? this->bind_obj : TUPLE_GET(args, 0);
+            if (this->const_args) {
+                res = ((const_unaryfunc)this->cfunc)(arg0);
+            } else {
+                res = ((unaryfunc)this->cfunc)(arg0);
+            }
+            break;
+        }
+        case 2: {
+            VSObject *arg0 = is_method ? this->bind_obj : TUPLE_GET(args, 0);
+            VSObject *arg1 = is_method ? TUPLE_GET(args, 0) : TUPLE_GET(args, 1);
+            if (this->const_args) {
+                res = ((const_binaryfunc)this->cfunc)(arg0, arg1);
+            } else {
+                res = ((binaryfunc)this->cfunc)(arg0, arg1);
+            }
+            break;
+        }
+        case 3: {
+            VSObject *arg0 = is_method ? this->bind_obj : TUPLE_GET(args, 0);
+            VSObject *arg1 = is_method ? TUPLE_GET(args, 0) : TUPLE_GET(args, 1);
+            VSObject *arg2 = is_method ? TUPLE_GET(args, 1) : TUPLE_GET(args, 2);
+            if (this->const_args) {
+                res = ((const_ternaryfunc)this->cfunc)(arg0, arg1, arg2);
+            } else {
+                res = ((ternaryfunc)this->cfunc)(arg0, arg1, arg2);
+            }
+            break;
+        }
+        case 4: {
+            VSObject *arg0 = is_method ? this->bind_obj : TUPLE_GET(args, 0);
+            VSObject *arg1 = is_method ? TUPLE_GET(args, 0) : TUPLE_GET(args, 1);
+            VSObject *arg2 = is_method ? TUPLE_GET(args, 1) : TUPLE_GET(args, 2);
+            VSObject *arg3 = is_method ? TUPLE_GET(args, 2) : TUPLE_GET(args, 3);
+            if (this->const_args) {
+                res = ((const_quaternaryfunc)this->cfunc)(arg0, arg1, arg2, arg3);
+            } else {
+                res = ((quaternaryfunc)this->cfunc)(arg0, arg1, arg2, arg3);
+            }
+            break;
+        }
+        default:
+            // Unreachable
+            res = NEW_REF(VSObject *, VS_NONE);
+            break;
+    }
+    return res;
+}
+
+/* Dynamic function type definition */
+VSDynamicFunctionObject::VSDynamicFunctionObject(VSStringObject *name, VSCodeObject *code, VSTupleObject *cellvars, VSTupleObject *freevars, VSTupleObject *builtins) {
+    this->name = NEW_REF(VSStringObject *, name);
+    this->code = NEW_REF(VSCodeObject *, code);
+    this->cellvars = NEW_REF(VSTupleObject *, cellvars);
+    this->freevars = NEW_REF(VSTupleObject *, freevars);
+    this->builtins = NEW_REF(VSTupleObject *, builtins);
+}
+
+VSDynamicFunctionObject::~VSDynamicFunctionObject() {
+    DECREF_EX(this->name);
+    DECREF_EX(this->code);
+    DECREF_EX(this->cellvars);
+    DECREF_EX(this->freevars);
+    DECREF_EX(this->builtins);
+}
+
+VSObject *VSDynamicFunctionObject::call(VSTupleObject *args) {
+    assert(args != NULL);
+    cint_t nargs = TUPLE_LEN(args);
+    if (nargs != this->code->nargs) {
+        ERR_NARGS(this->code->nargs, nargs);
+    }
+
+    std::stack<VSObject *> stack = std::stack<VSObject *>();
+    VSFrameObject *frame = new VSFrameObject(this->code, args, this->cellvars, this->freevars, this->builtins, NULL);
+    frame->eval(stack);
+
+    if (stack.empty()) {
+        INCREF_RET(VS_NONE);
+    } else if (stack.size() == 1) {
+        return stack.top();
+    } else {
+        err("Internal error: more than 1 object left in compute stack: %ld", stack.size());
+        terminate(TERM_ERROR);
+    }
+}
