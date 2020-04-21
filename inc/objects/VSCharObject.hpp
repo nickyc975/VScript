ad7b2d7bd5ad09e7ac740d0a2fbeeaf2551cc724
nickyc975 <chenjinlong2016@gmail.com>
2020-04-21 14:23:09 +0800

    Refactored VSCharObject.

--- inc/objects/VSCharObject.hpp
+++ inc/objects/VSCharObject.hpp
@@ -2,17 +2,15 @@
 #define VS_CHAR_H
 
 #include "VSObject.hpp"
-#include "VSTypeObject.hpp"
 
-extern VSTypeObject *VSCharType;
+VSObject *vs_char(VSObject *obj);
 
-class VSCharObject : public VSObject
-{
+class VSCharObject : public VSObject {
 public:
     const cchar_t _value;
 
-    VSCharObject() : _value(0) { this->type = VSCharType; }
-    VSCharObject(cchar_t val) : _value(val) { this->type = VSCharType; }
+    VSCharObject(cchar_t value);
+    ~VSCharObject();
 };
 
 #define AS_CHAR(obj) ((VSCharObject *)obj)
--- src/objects/VSCharObject.cpp
+++ src/objects/VSCharObject.cpp
@@ -1,174 +1,106 @@
-#include "error.hpp"
-#include "objects/VSIntObject.hpp"
-#include "objects/VSFloatObject.hpp"
-#include "objects/VSBoolObject.hpp"
 #include "objects/VSCharObject.hpp"
+
+#include "error.hpp"
+#include "objects/VSBoolObject.hpp"
+#include "objects/VSFloatObject.hpp"
+#include "objects/VSFunctionObject.hpp"
+#include "objects/VSIntObject.hpp"
 #include "objects/VSStringObject.hpp"
 #include "objects/VSTupleObject.hpp"
 
-VSObject *vs_char_new(VSObject *typeobj, VSObject *args, VSObject *)
-{
-    VSTypeObject *ttype = VS_TYPEOF(typeobj);
-    VS_ENSURE_TYPE(ttype, T_TYPE, "char new");
-
-    VSTypeObject *type = VS_AS_TYPE(typeobj);
-    VS_ENSURE_TYPE(type, T_CHAR, "char new");
-
-    vs_size_t len = TUPLE_LEN(args);
-    if (len == 0)
-    {
-        INCREF_RET(new VSCharObject());
-    }
-    else if (len > 1)
-    {
-        err("char.__new__() expected 0 or 1 arg but got %llu.", len);
+VSObject *vs_char(VSObject *obj) {
+    if (!HAS_ATTR(obj, "__char__")) {
+        ERR_ATTR_MISSING(obj, "__char__");
         terminate(TERM_ERROR);
     }
 
-    VSObject *init_val = TUPLE_GET(args, 0);
-    VSTypeObject *init_type = VS_TYPEOF(init_val);
-    if (init_type->_number_funcs == NULL || init_type->_number_funcs->__char__ == NULL)
-    {
-        err("can not cast type \"%s\" to type \"char\".", init_type->__name__.c_str());
+    VSObject *char_func = GET_ATTR(obj, "__char__");
+    if (!VS_IS_TYPE(char_func, T_FUNC)) {
+        ERR_ATTR_IS_NOT_FUNC(obj, "__char__");
         terminate(TERM_ERROR);
     }
 
-    VSObject *val = init_type->_number_funcs->__char__(init_val);
-    if (VS_TYPEOF(val)->t_type != T_CHAR)
-    {
-        err("%s.__char__() returned \"%s\" instead of char.", init_type->__name__.c_str(), VS_TYPEOF(val)->__name__.c_str());
+    VSObject *val = ((VSFunctionObject *)char_func)->call(vs_tuple_pack(0));
+    if (!VS_IS_TYPE(val, T_CHAR)) {
+        err("%s.__char__() returned \"%s\" instead of char.", TYPE_STR[obj->type], TYPE_STR[val->type]);
         terminate(TERM_ERROR);
     }
 
-    DECREF_EX(init_val);
     INCREF_RET(val);
 }
 
-void vs_char_init(VSObject *self, VSObject *args, VSObject *)
-{
-}
-
-VSObject *vs_char_copy(const VSObject *charobj)
-{
-    VSTypeObject *type = VS_TYPEOF(charobj);
-    VS_ENSURE_TYPE(type, T_CHAR, "char copy");
-
-    VSCharObject *oldchar = (VSCharObject *)charobj;
-    VSCharObject *newchar = new VSCharObject(oldchar->_value);
-    INCREF_RET(VS_AS_OBJECT(newchar));
-}
-
-VSObject *vs_char_hash(const VSObject *charobj)
-{
-    VSTypeObject *type = VS_TYPEOF(charobj);
-
-    VS_ENSURE_TYPE(type, T_CHAR, "char hash");
+VSObject *vs_char_hash(const VSObject *charobj) {
+    VS_ENSURE_TYPE(charobj, T_CHAR, "char.__hash__()");
 
     INCREF_RET(C_INT_TO_INT((((VSCharObject *)charobj)->_value)));
 }
 
-VSObject *vs_char_lt(const VSObject *a, const VSObject *b)
-{
-    VSTypeObject *a_type = VS_TYPEOF(a);
-
-    VS_ENSURE_TYPE(a_type, T_BOOL, "char lt");
-
-    VSTypeObject *b_type = VS_TYPEOF(b);
-
-    VS_ENSURE_TYPE(b_type, T_BOOL, "char lt");
+VSObject *vs_char_lt(const VSObject *a, const VSObject *b) {
+    VS_ENSURE_TYPE(a, T_BOOL, "char.__lt__()");
+    VS_ENSURE_TYPE(b, T_BOOL, "char.__lt__()");
 
     cbool_t res = ((VSCharObject *)a)->_value < ((VSCharObject *)b)->_value;
     INCREF_RET(res ? VS_TRUE : VS_FALSE);
 }
 
-VSObject *vs_char_eq(const VSObject *a, const VSObject *b)
-{
-    VSTypeObject *a_type = VS_TYPEOF(a);
-
-    VS_ENSURE_TYPE(a_type, T_BOOL, "char eq");
-
-    VSTypeObject *b_type = VS_TYPEOF(b);
-
-    VS_ENSURE_TYPE(b_type, T_BOOL, "char eq");
+VSObject *vs_char_eq(const VSObject *a, const VSObject *b) {
+    VS_ENSURE_TYPE(a, T_BOOL, "char.__eq__()");
+    VS_ENSURE_TYPE(b, T_BOOL, "char.__eq__()");
 
     bool res = ((VSCharObject *)a)->_value == ((VSCharObject *)b)->_value;
     INCREF_RET(res ? VS_TRUE : VS_FALSE);
 }
 
-VSObject *vs_char_str(VSObject *charobj)
-{
-    VSTypeObject *type = VS_TYPEOF(charobj);
-    VS_ENSURE_TYPE(type, T_CHAR, "char to str");
+VSObject *vs_char_str(VSObject *charobj) {
+    VS_ENSURE_TYPE(charobj, T_CHAR, "char.__str__()");
 
     auto str = std::string();
     str.push_back(((VSCharObject *)charobj)->_value);
     INCREF_RET(C_STRING_TO_STRING((str)));
 }
 
-VSObject *vs_char_bytes(VSObject *charobj)
-{
-    VSTypeObject *type = VS_TYPEOF(charobj);
+VSObject *vs_char_bytes(VSObject *charobj) {
+    VS_ENSURE_TYPE(charobj, T_CHAR, "char.__bytes__()");
 
-    VS_ENSURE_TYPE(type, T_CHAR, "char to bytes");
-
-    return NULL;
+    INCREF_RET(VS_NONE);
 }
 
-VSObject *vs_char_neg(VSObject *charobj)
-{
-    VSTypeObject *type = VS_TYPEOF(charobj);
-    VS_ENSURE_TYPE(type, T_CHAR, "char.__neg__()");
+VSObject *vs_char_neg(VSObject *charobj) {
+    VS_ENSURE_TYPE(charobj, T_CHAR, "char.__neg__()");
 
     cchar_t res = -((VSCharObject *)charobj)->_value;
     INCREF_RET(C_CHAR_TO_CHAR(res));
 }
 
-VSObject *vs_char_add(VSObject *a, VSObject *b)
-{
-    VSTypeObject *atype = VS_TYPEOF(a);
-    VS_ENSURE_TYPE(atype, T_CHAR, "char.__add__()");
-
-    VSTypeObject *btype = VS_TYPEOF(b);
-    VS_ENSURE_TYPE(btype, T_CHAR, "char.__add__()");
+VSObject *vs_char_add(VSObject *a, VSObject *b) {
+    VS_ENSURE_TYPE(a, T_CHAR, "char.__add__()");
+    VS_ENSURE_TYPE(b, T_CHAR, "char.__add__()");
 
     cchar_t res = ((VSCharObject *)a)->_value + ((VSCharObject *)b)->_value;
     INCREF_RET(C_CHAR_TO_CHAR(res));
 }
 
-VSObject *vs_char_sub(VSObject *a, VSObject *b)
-{
-    VSTypeObject *atype = VS_TYPEOF(a);
-    VS_ENSURE_TYPE(atype, T_CHAR, "char.__sub__()");
-
-    VSTypeObject *btype = VS_TYPEOF(b);
-    VS_ENSURE_TYPE(btype, T_CHAR, "char.__sub__()");
+VSObject *vs_char_sub(VSObject *a, VSObject *b) {
+    VS_ENSURE_TYPE(a, T_CHAR, "char.__sub__()");
+    VS_ENSURE_TYPE(b, T_CHAR, "char.__sub__()");
 
     cchar_t res = ((VSCharObject *)a)->_value - ((VSCharObject *)b)->_value;
     INCREF_RET(C_CHAR_TO_CHAR(res));
 }
 
-VSObject *vs_char_mul(VSObject *a, VSObject *b)
-{
-    VSTypeObject *atype = VS_TYPEOF(a);
-    VS_ENSURE_TYPE(atype, T_CHAR, "char.__mul__()");
-
-    VSTypeObject *btype = VS_TYPEOF(b);
-    VS_ENSURE_TYPE(btype, T_CHAR, "char.__mul__()");
+VSObject *vs_char_mul(VSObject *a, VSObject *b) {
+    VS_ENSURE_TYPE(a, T_CHAR, "char.__mul__()");
+    VS_ENSURE_TYPE(b, T_CHAR, "char.__mul__()");
 
     cchar_t res = ((VSCharObject *)a)->_value * ((VSCharObject *)b)->_value;
     INCREF_RET(C_CHAR_TO_CHAR(res));
 }
 
-VSObject *vs_char_div(VSObject *a, VSObject *b)
-{
-    VSTypeObject *atype = VS_TYPEOF(a);
-    VS_ENSURE_TYPE(atype, T_CHAR, "char.__div__()");
+VSObject *vs_char_div(VSObject *a, VSObject *b) {
+    VS_ENSURE_TYPE(a, T_CHAR, "char.__div__()");
+    VS_ENSURE_TYPE(b, T_CHAR, "char.__div__()");
 
-    VSTypeObject *btype = VS_TYPEOF(b);
-    VS_ENSURE_TYPE(btype, T_CHAR, "char.__div__()");
-
-    if (((VSCharObject *)b)->_value == 0)
-    {
+    if (((VSCharObject *)b)->_value == 0) {
         err("divided by zero\n");
         terminate(TERM_ERROR);
     }
@@ -177,16 +109,11 @@ VSObject *vs_char_div(VSObject *a, VSObject *b)
     INCREF_RET(C_CHAR_TO_CHAR(res));
 }
 
-VSObject *vs_char_mod(VSObject *a, VSObject *b)
-{
-    VSTypeObject *atype = VS_TYPEOF(a);
-    VS_ENSURE_TYPE(atype, T_CHAR, "char.__mod__()");
+VSObject *vs_char_mod(VSObject *a, VSObject *b) {
+    VS_ENSURE_TYPE(a, T_CHAR, "char.__mod__()");
+    VS_ENSURE_TYPE(b, T_CHAR, "char.__mod__()");
 
-    VSTypeObject *btype = VS_TYPEOF(b);
-    VS_ENSURE_TYPE(btype, T_CHAR, "char.__mod__()");
-
-    if (((VSCharObject *)b)->_value == 0)
-    {
+    if (((VSCharObject *)b)->_value == 0) {
         err("mod by zero\n");
         terminate(TERM_ERROR);
     }
@@ -195,80 +122,51 @@ VSObject *vs_char_mod(VSObject *a, VSObject *b)
     INCREF_RET(C_CHAR_TO_CHAR(res));
 }
 
-VSObject *vs_char_bool(VSObject *charobj)
-{
-    VSTypeObject *type = VS_TYPEOF(charobj);
-    VS_ENSURE_TYPE(type, T_CHAR, "__bool__()");
+VSObject *vs_char_bool(VSObject *charobj) {
+    VS_ENSURE_TYPE(charobj, T_CHAR, "char.__bool__()");
 
     cchar_t val = ((VSCharObject *)charobj)->_value;
     INCREF_RET(val ? VS_TRUE : VS_FALSE);
 }
 
-VSObject *vs_char_char(VSObject *charobj)
-{
-    VSTypeObject *type = VS_TYPEOF(charobj);
-    VS_ENSURE_TYPE(type, T_CHAR, "__char__()");
+VSObject *vs_char_char(VSObject *charobj) {
+    VS_ENSURE_TYPE(charobj, T_CHAR, "char.__char__()");
 
     INCREF_RET(charobj);
 }
 
-VSObject *vs_char_int(VSObject *charobj, VSObject *base)
-{
-    VSTypeObject *type = VS_TYPEOF(charobj);
-    VS_ENSURE_TYPE(type, T_CHAR, "__int__()");
-
-    if (base != NULL)
-    {
-        err("char.__int__() expected 1 arg but got 2\n");
-        terminate(TERM_ERROR);
-    }
+VSObject *vs_char_int(VSObject *charobj) {
+    VS_ENSURE_TYPE(charobj, T_CHAR, "char.__int__()");
 
     INCREF_RET(C_INT_TO_INT((cint_t)CHAR_TO_C_CHAR(charobj)));
 }
 
-VSObject *vs_char_float(VSObject *charobj)
-{
-    VSTypeObject *type = VS_TYPEOF(charobj);
-    VS_ENSURE_TYPE(type, T_CHAR, "__float__()");
+VSObject *vs_char_float(VSObject *charobj) {
+    VS_ENSURE_TYPE(charobj, T_CHAR, "char.__float__()");
 
     INCREF_RET(C_FLOAT_TO_FLOAT(((cfloat_t)CHAR_TO_C_CHAR(charobj))));
 }
 
-NumberFuncs *char_number_funcs = new NumberFuncs(
-    NULL,         // __not__
-    vs_char_neg,  // __neg__
-    vs_char_add,  // __add__
-    vs_char_sub,  // __sub__
-    vs_char_mul,  // __mul__
-    vs_char_div,  // __div__
-    vs_char_mod,  // __mod__
-    NULL,         // __and__
-    NULL,         // __or__
-    vs_char_bool, // __bool__
-    vs_char_char, // __char__
-    vs_char_int,  // __int__
-    vs_char_float // __float__
-);
+VSCharObject::VSCharObject(cchar_t value) {
+    this->type = T_CHAR;
+    this->_value = value;
 
-VSTypeObject *VSCharType = new VSTypeObject(
-    VSTypeType,
-    T_CHAR,
-    "char",            // __name__
-    NULL,              // __attrs__
-    vs_char_new,       // __new__
-    vs_char_init,      // __init__
-    vs_char_copy,      // __copy__
-    NULL,              // __clear__
-    NULL,              // __getattr__
-    NULL,              // __hasattr__
-    NULL,              // __setattr__
-    NULL,              // __removeattr__
-    vs_char_hash,      // __hash__
-    vs_char_lt,        // __lt__
-    vs_char_eq,        // __eq__
-    vs_char_str,       // __str__
-    vs_char_bytes,     // __bytes__
-    NULL,              // __call__
-    char_number_funcs, // _number_funcs
-    NULL               // _container_funcs
-);
+    NEW_NATIVE_FUNC_ATTR(this, "__hash__", vs_char_hash, 1, true);
+    NEW_NATIVE_FUNC_ATTR(this, "__lt__", vs_char_lt, 2, true);
+    NEW_NATIVE_FUNC_ATTR(this, "__eq__", vs_char_eq, 2, true);
+    NEW_NATIVE_FUNC_ATTR(this, "__str__", vs_char_str, 1, false);
+    NEW_NATIVE_FUNC_ATTR(this, "__bytes__", vs_char_bytes, 1, false);
+    NEW_NATIVE_FUNC_ATTR(this, "__neg__", vs_char_neg, 1, false);
+    NEW_NATIVE_FUNC_ATTR(this, "__add__", vs_char_add, 2, false);
+    NEW_NATIVE_FUNC_ATTR(this, "__sub__", vs_char_sub, 2, false);
+    NEW_NATIVE_FUNC_ATTR(this, "__mul__", vs_char_mul, 2, false);
+    NEW_NATIVE_FUNC_ATTR(this, "__div__", vs_char_div, 2, false);
+    NEW_NATIVE_FUNC_ATTR(this, "__mod__", vs_char_mod, 2, false);
+    NEW_NATIVE_FUNC_ATTR(this, "__bool__", vs_char_bool, 1, false);
+    NEW_NATIVE_FUNC_ATTR(this, "__char__", vs_char_char, 1, false);
+    NEW_NATIVE_FUNC_ATTR(this, "__int__", vs_char_int, 1, false);
+    NEW_NATIVE_FUNC_ATTR(this, "__float__", vs_char_float, 1, false);
+}
+
+VSCharObject::~VSCharObject() {
+}
